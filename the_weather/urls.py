7ed52ec3c69572e344a424eeb4c38f048934c396
sbm1arora <shubham.arora4598@gmail.com>
2021-02-09 17:05:10 +0530

    Changing to just entering a city and getting weather of the entered city

--- the_weather/urls.py
+++ the_weather/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from weather.views import index, show_weather
+from weather import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", index),
-    path("city", show_weather),
+    path("", views.index, name="index"),
 ]
--- weather/views.py
+++ weather/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.http import HttpResponse
 from .models import City
 from .forms import CityForm
@@ -8,35 +8,25 @@ import requests
 # Create your views here.
 
 
-def show_weather(request):
-    weather_data = []
-    cities = City.objects.all()
-    for city in cities:
-        url = f"http://api.openweathermap.org/data/2.5/weather?q={city.name}&appid=277607675933f3fb5143251f8a1c85ee&units=metric"
-        res = requests.get(url)
-        data = res.json()
-
-        city_weather = {
-            "city": city.name,
-            "description": data["weather"][0]["description"],
-            "temprature": data["main"]["temp"],
-            "wind": data["wind"]["speed"],
-            "icon": data["weather"][0]["icon"],
-        }
-        weather_data.append(city_weather)
-    context = {"weather": weather_data}
-
-    return render(request, "weather/city.html", context)
-
-
 def index(request):
     if request.method == "POST":
         form = CityForm(request.POST or None)
         if form.is_valid():
-            form = CityForm()
-        return redirect("show_weather")
+            city_name = form.cleaned_data["city"]
+            url = f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid=277607675933f3fb5143251f8a1c85ee&units=metric"
+            res = requests.get(url)
+            data = res.json()
+
+            city_weather = {
+                "city": city_name,
+                "description": data["weather"][0]["description"],
+                "temprature": data["main"]["temp"],
+                "wind": data["wind"]["speed"],
+                "icon": data["weather"][0]["icon"],
+            }
+            context = {"weather": city_weather, "form": form}
+        return render(request, "weather/city.html", context)
     else:
         form = CityForm()
-
-    return render(request, "weather/index.html", {"form": form})
+        return render(request, "weather/index.html", {"form": form})
 
